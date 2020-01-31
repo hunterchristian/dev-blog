890a74eb9f9b2a4b6741105db51ed23f48e1f663
Hunter Hodnett <hchodnett@gmail.com>
2020-01-30 20:05:36 -0600

    Add profile picture and bio

--- config.js
+++ config.js
@@ -3,8 +3,9 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Dev thoughts by Hunter',
+  subtitle:
+    "All of this is only here so that I don't make the same mistake twice",
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -13,21 +14,22 @@ module.exports = {
   menu: [
     {
       label: 'Articles',
-      path: '/'
+      path: '/',
     },
     {
       label: 'About me',
-      path: '/pages/about'
+      path: '/pages/about',
     },
     {
       label: 'Contact me',
-      path: '/pages/contacts'
-    }
+      path: '/pages/contacts',
+    },
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'Hunter Hodnett',
+    photo: '/mug.jpg',
+    bio:
+      'Listened to great tunes at Amazon Music.\nLaughed at memes at Reddit.\nNow I teach software engineering at Orange Method.',
     contacts: {
       email: '',
       facebook: '#',
@@ -44,6 +46,6 @@ module.exports = {
       codepen: '',
       youtube: '',
       soundcloud: '',
-    }
-  }
+    },
+  },
 };
